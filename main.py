03347a3d2cb0fe4d179d7bd2ca385958072b5476
maher-cshub <maherarab.1299@gmail.com>
2023-09-07 14:16:13 +0100

    feat: implementing header

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import flet as ft
 
-def main(page: ft.Page):
-    # add/update controls on Page
-    pass
 
-ft.app(target=main)
+def main(page: ft.Page):
+    def check_item_clicked(e):
+        e.control.checked = not e.control.checked
+        page.update()
+
+    page.appbar = ft.AppBar(
+        leading_width=40,
+        title=ft.Text("HTML Email Sender"),
+        center_title=False,
+        bgcolor=ft.colors.CYAN_300,
+        actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+        ],
+    )
+    page.add(ft.Text("Body!"))
+
+ft.app(port=8550, target=main, view=ft.WEB_BROWSER)
